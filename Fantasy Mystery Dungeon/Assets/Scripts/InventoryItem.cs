0b956047d3eb8b7646be2a526b641970551f74e1
Roger <roger@dalkefamily.com>
2020-06-19 00:57:14 -0700

    Added InventoryItem
    Added a scriptable object for InventoryItems and added a small health potion and small potion bottle as test InventoryItems.

---
+++ Fantasy Mystery Dungeon/Assets/Scripts/InventoryItem.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+[CreateAssetMenu(fileName = "NewInventoryItem", menuName = "ScriptableObjects/InventoryItem", order = 0)]
+public class InventoryItem : ScriptableObject
+{
+    public string itemName;
+    public Sprite itemSprite;
+    public int value;
+}
