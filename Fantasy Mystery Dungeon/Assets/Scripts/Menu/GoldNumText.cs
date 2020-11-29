34cf1a150ddce793e3c7b6fa2f36e399874035c1
Roger <roger@dalkefamily.com>
2020-11-28 20:16:02 -0800

    Added UI element and script to display gold (currency)
    Added script that sets Text.text to number of gold the player has. Implemented in Hub.

---
+++ Fantasy Mystery Dungeon/Assets/Scripts/Menu/GoldNumText.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Text))]
+public class GoldNumText : MonoBehaviour
+{
+	private Text goldText;
+
+	private void Awake()
+	{
+		goldText = GetComponent<Text>();
+	}
+	private void FixedUpdate ()
+	{
+		goldText.text = PlayerInventory.gold.ToString();
+	}
+}
